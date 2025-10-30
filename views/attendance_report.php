6d0ea9e6dc188cedf2d3bfe5e63873eb852506d2
Owell26 <157805814+Owell26@users.noreply.github.com>
2025-10-30 09:26:59 +0800

    Refactor to calculate days in month using date function
    Replaced cal_days_in_month function with date function to get the number of days in the selected month.

--- views/attendance_report.php
+++ views/attendance_report.php
@@ -77,9 +77,10 @@ include '../auth/authentication.php';
             $year_section = mysqli_real_escape_string($conn, $_GET['year_section']);
             $month = isset($_GET['month']) ? intval($_GET['month']) : date('n');
             $year = date('Y');
+          
+            // Get number of days in the selected month (works even without calendar extension)
+            $num_days = date('t', strtotime("$year-$month-01"));
 
-            // Get number of days in the selected month
-            $num_days = cal_days_in_month(CAL_GREGORIAN, $month, $year);
 
             // Get students in the section who have attendance records from this device
             $students_sql = "
